de327b84936ae4ba52db337b20020bb82efbc3d5
fatemehashtari <fatemehashtari25@gmail.com>
2025-05-31 17:32:16 +0330

    Add files via upload

--- app.py
+++ app.py
@@ -115,8 +115,8 @@ def test():
         return render_template('login.html')
     except Exception as e:
         return f"Error: {str(e)}", 500
-
+init_db()
+port = int(os.environ.get('PORT', 5000))
 if __name__ == '__main__':
-    init_db()
-    port = int(os.environ.get('PORT', 5000))
+
     app.run(host='0.0.0.0', port=port)
